7354c8b1d6dea738dca8bd8e1c03ee1c04a2d9ac
Alastair Smith <Alastair-smith2@users.noreply.github.com>
2021-12-03 01:06:55 +0000

    Title: Delete old files
    Body: Delete files that have been removed

--- heap/typescript/src/interface.ts
+++
@@ -1,8 +0,0 @@
-export interface PriorityQueue<T> {
-  top: () => T | null;
-  peek: () => T | null;
-  insert: (element: T, priority: number) => boolean;
-  remove: (element: T) => boolean;
-  update: (element: T, newValue: T) => boolean;
-  contains: (element: T) => boolean;
-}
